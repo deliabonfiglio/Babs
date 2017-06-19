d2bed3fabefe2d2955e5ddc413b1256c62fb7b4f
Delia <Delia@PcStudio>
2017-06-19 21:53:00 +0200

    fine

--- src/it/polito/tdp/babs/BabsController.java
+++ src/it/polito/tdp/babs/BabsController.java
@@ -74,7 +74,6 @@ public class BabsController {
 		simulazione.loadStations(kvalue, model.getStations());
 		simulazione.run();
 		
-		
 		//devo prendere i risultati della simulazione appena fatta, altrimenti ne creerei una nuova!
 		txtResult.clear();
 		
